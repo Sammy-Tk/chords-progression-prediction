56b16a19ad20e0aae11eba2cb649961dcd405410
Emily Cardwell <emily@emilycardwell.com>
2022-12-06 10:11:36 +0100

    separated preprocessor from model py files

--- chords_prog_proj/interface/main.py
+++ chords_prog_proj/interface/main.py
@@ -8,84 +8,13 @@ from colorama import Fore, Style
 
 from tensorflow.keras.utils import to_categorical
 
-from chords_prog_proj.ml_logic.data import clean_data, get_chunk, save_chunk
 from chords_prog_proj.ml_logic.model import initialize_model, compile_model, train_model, evaluate_model
 from chords_prog_proj.ml_logic.params import CHUNK_SIZE, DATASET_SIZE, VALIDATION_DATASET_SIZE, LOCAL_DATA_PATH, DATA_FILE_KAGGLE, DATA_FILE_JAZZ
-from chords_prog_proj.ml_logic.preprocessor import preprocess_features
-from chords_prog_proj.ml_logic.utils import get_dataset_timestamp
 from chords_prog_proj.ml_logic.registry import get_model_version
+from chords_prog_proj.ml_logic.preprocessor import preprocess
 
 from chords_prog_proj.ml_logic.registry import load_model, save_model
 
-
-def preprocess(source_type = 'train'):
-    """
-    Preprocess the dataset by chunks fitting in memory.
-    parameters:
-    - source_type: 'train' or 'val'
-    """
-
-    print("\n⭐️ Use case: preprocess")
-
-    # Iterate on the dataset, in chunks
-    chunk_id = 0
-    row_count = 0
-    cleaned_row_count = 0
-    source_name = f"{source_type}_{DATASET_SIZE}"
-    destination_name = f"{source_type}_processed_{DATASET_SIZE}"
-
-    while (True):
-        print(Fore.BLUE + f"\nProcessing chunk n°{chunk_id}..." + Style.RESET_ALL)
-
-        data_chunk = get_chunk(
-            source_name=source_name,
-            index=chunk_id * CHUNK_SIZE,
-            chunk_size=CHUNK_SIZE
-        )
-
-        # Break out of while loop if data is none
-        if data_chunk is None:
-            print(Fore.BLUE + "\nNo data in latest chunk..." + Style.RESET_ALL)
-            break
-
-        row_count += data_chunk.shape[0]
-
-        data_chunk_cleaned = clean_data(data_chunk)
-
-        cleaned_row_count += len(data_chunk_cleaned)
-
-        # Break out of while loop if cleaning removed all rows
-        if len(data_chunk_cleaned) == 0:
-            print(Fore.BLUE + "\nNo cleaned data in latest chunk..." + Style.RESET_ALL)
-            break
-
-        X_chunk = data_chunk_cleaned.drop("fare_amount", axis=1)
-        y_chunk = data_chunk_cleaned[["fare_amount"]]
-
-        X_processed_chunk = preprocess_features(X_chunk)
-
-        data_processed_chunk = pd.DataFrame(
-            np.concatenate((X_processed_chunk, y_chunk), axis=1)
-        )
-
-        # Save and append the chunk
-        is_first = chunk_id == 0
-
-        save_chunk(
-            destination_name=destination_name,
-            is_first=is_first,
-            data=data_processed_chunk
-        )
-
-        chunk_id += 1
-
-    if row_count == 0:
-        print("\n✅ No new data for the preprocessing 👌")
-        return None
-
-    print(f"\n✅ Data processed saved entirely: {row_count} rows ({cleaned_row_count} cleaned)")
-
-    return None
 
 def train():
     """
