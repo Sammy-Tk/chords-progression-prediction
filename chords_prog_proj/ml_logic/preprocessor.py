53cf144d60145df5c7eb2ba6afb17baeba098fad
Emily Cardwell <emily@emilycardwell.com>
2022-12-06 11:47:33 +0100

    edited preprocessor, utils, and data .py files in ml_logic

--- chords_prog_proj/ml_logic/preprocessor.py
+++ chords_prog_proj/ml_logic/preprocessor.py
@@ -1,47 +1,101 @@
-from sklearn.pipeline import make_pipeline
-from sklearn.compose import ColumnTransformer, make_column_transformer
-from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
-
-# from taxifare.ml_logic.encoders import (transform_time_features,
-#                                               transform_lonlat_features,
-#                                               compute_geohash)
-
-import numpy as np
 import pandas as pd
+import os
 
-from colorama import Fore, Style
+from chords_prog_proj.ml_logic.data import get_csv_data, get_text_data, expand_cols, \
+            drop_cols, clean_chords, new_columns, song_length, genre_cleaning, \
+            filter_length, df_to_csv
+from chords_prog_proj.ml_logic.utils import count_chords, count_artists, count_genres
 
 
-def preprocess_features(X: pd.DataFrame) -> np.ndarray:
+'''
+READ DATA, PRE-CLEANING ENGINEERING AND CONCAT
+'''
+def pre_clean():
+    # read data
+    raw_kaggle_df = get_csv_data()
+    raw_jazz_col = get_text_data()
+
+    # merge
+    raw_jazz_df = expand_cols(raw_jazz_col)
+    slim_kaggle_df = drop_cols(raw_kaggle_df)
+    concat_df = pd.concat([slim_kaggle_df, raw_jazz_df], ignore_index=True)
+
+    print(f'\n✅ Data read and merged, with {len(concat_df)} rows.')
+
+    return concat_df
 
 
+'''
+CLEAN
+'''
+def clean(concat_df):
+    cleaned_df = concat_df.copy()
+
+    cleaned_chords_column = clean_chords(cleaned_df['chords'])
+    cleaned_df['chords'] = cleaned_chords_column
+
+    print(f'\n✅ Data cleaned, {len(cleaned_df)} of {len(concat_df)} \
+        rows kept during cleaning.')
+
+    return cleaned_df
 
 
-    print(Fore.BLUE + "\nPreprocess features..." + Style.RESET_ALL)
-
-    preprocessor = create_sklearn_preprocessor()
-
-    X_processed = preprocessor.fit_transform(X)
-
-    print("\n✅ X_processed, with shape", X_processed.shape)
-
-    return X_processed
-
-def preprocess(source_type = 'train'):
-    """
-    Preprocess the dataset by chunks fitting in memory.
-    parameters:
-    - source_type: 'train' or 'val'
-    """
+'''
+PREPROCESS
+'''
+def preprocess(get_distributions=False):
 
     print("\n⭐️ Use case: preprocess")
 
+    concat_df = pre_clean()
 
+    cleaned_df = clean(concat_df)
 
-    X_processed_chunk = preprocess_features(X_chunk)
+    # drop duplicates based on chords and song name
+    new_columns_df = new_columns(cleaned_df)
 
+    unreplicated_df = \
+        new_columns_df.drop_duplicates(subset=['chords_list', 'song_name'],
+                                    keep = 'last').reset_index(drop = True)
 
+    # drop unnecessary columns
+    unreplicated_df.drop(columns=['song_name', 'chords_list'], inplace=True)
 
-    print(f"\n✅ Data processed saved entirely: {row_count} rows ({cleaned_row_count} cleaned)")
+    print(f'\n✅ Genre cleaning... may take up to 1 minute.')
+
+    # clean genres
+    slim_genres = genre_cleaning(unreplicated_df['genres'])
+    clean_genres_df = unreplicated_df.copy()
+    clean_genres_df['genres'] = slim_genres
+
+    # get song length column, and filter by length
+    song_len_df = song_length(clean_genres_df)
+    final_df = filter_length(song_len_df, 8)
+
+    if get_distributions == False:
+        pass
+    else:
+        print(f'\n✅ Top five chords, grenres, and artists being generated.')
+        chord_count_df = count_chords(final_df, low_freq_to_remove=10,
+                                      histplot=True, ascending=False)
+        print(chord_count_df.head(5))
+        genre_count_df = count_genres(final_df, histplot=True)
+        print(genre_count_df.head(5))
+        artists_df = count_artists(final_df, histplot=True)
+        print(artists_df.head(5))
+
+    # send to csv
+    root_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    data_proc_folder = os.path.join(root_path, 'chords_prog_proj/mlops/data/processed')
+
+    i = 1
+    for file in os.listdir(data_proc_folder):
+        if i in file:
+            pass
+            i += 1
+        else:
+            df_to_csv(final_df, i, data_proc_folder)
+
+    print(f'\n✅ Preprocessor finished.')
 
     return None
