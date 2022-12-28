4c6beb34debe3c5ad77210cc761c210c494ab738
Sammy El Takriti <seltakriti@gmail.com>
2022-12-28 17:43:58 +0100

    Remove unused code from taxifare challenge

--- chords_prog_proj/api/fast.py
+++ chords_prog_proj/api/fast.py
@@ -34,15 +34,32 @@ def predict_baseline(chords: str):
 
 
 @app.get("/predict")
-def predict(song, n_chords, randomness):
+def predict(song: list,
+            n_chords: int,
+            randomness: int):
     """
     Make a prediction using the latest trained model
     """
+
+    """
+    we use type hinting to indicate the data types expected
+    for the parameters of the function
+    FastAPI uses this information in order to hand errors
+    to the developpers providing incompatible parameters
+    FastAPI also provides variables of the expected data type to use
+    without type hinting we need to manually convert
+    the parameters of the functions which are all received as strings
+    """
+
     song = song.split(',')
     n_chords = int(n_chords)
     randomness = int(randomness)
 
     n_chords_pred = pred(song=song, n_chords=n_chords, randomness=randomness, model=app.state.model)
+
+    # ⚠️ fastapi only accepts simple python data types as a return value
+    # among which dict, list, str, int, float, bool
+    # in order to be able to convert the api response to json
 
     return {'predicted_chord': n_chords_pred}
 
@@ -52,58 +69,3 @@ def root():
     # $CHA_BEGIN
     return dict(greeting="Hello")
     # $CHA_END
-
-# @app.get("/predict")
-# def predict(pickup_datetime: datetime,  # 2013-07-06 17:18:00
-#             pickup_longitude: float,    # -73.950655
-#             pickup_latitude: float,     # 40.783282
-#             dropoff_longitude: float,   # -73.984365
-#             dropoff_latitude: float,    # 40.769802
-#             passenger_count: int):      # 1
-#     """
-#     we use type hinting to indicate the data types expected
-#     for the parameters of the function
-#     FastAPI uses this information in order to hand errors
-#     to the developpers providing incompatible parameters
-#     FastAPI also provides variables of the expected data type to use
-#     without type hinting we need to manually convert
-#     the parameters of the functions which are all received as strings
-#     """
-#     # $CHA_BEGIN
-
-#     # ⚠️ if the timezone conversion was not handled here the user would be assumed to provide an UTC datetime
-#     # create datetime object from user provided date
-#     # pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
-
-#     # localize the user provided datetime with the NYC timezone
-#     eastern = pytz.timezone("US/Eastern")
-#     localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
-
-#     # convert the user datetime to UTC and format the datetime as expected by the pipeline
-#     utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
-#     formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
-
-#     # fixing a value for the key, unused by the model
-#     # in the future the key might be removed from the pipeline input
-#     key = "2013-07-06 17:18:00.000000119"
-
-#     X_pred = pd.DataFrame(dict(
-#         key=[key],  # useless but the pipeline requires it
-#         pickup_datetime=[formatted_pickup_datetime],
-#         pickup_longitude=[pickup_longitude],
-#         pickup_latitude=[pickup_latitude],
-#         dropoff_longitude=[dropoff_longitude],
-#         dropoff_latitude=[dropoff_latitude],
-#         passenger_count=[passenger_count]))
-
-#     model = app.state.model
-#     X_processed = preprocess_features(X_pred)
-#     y_pred = model.predict(X_processed)
-
-#     # ⚠️ fastapi only accepts simple python data types as a return value
-#     # among which dict, list, str, int, float, bool
-#     # in order to be able to convert the api response to json
-#     return dict(fare=float(y_pred))
-#     # $CHA_END
-
-#EC
